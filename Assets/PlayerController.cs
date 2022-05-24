b5c96eb8ddef0df1f4f374958bc3967690a1982f
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-05-24 14:34:02 -0300

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+
+    public bool isAble;
+    public GameObject prefab;
+    private Vector3 newScale;
+
+    void Start()
+    {
+        isAble = false;
+        newScale = new Vector3(0.2f,0.3f,0.2f);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            //transform.position += new Vector3(0, 0, speed);
+            transform.Translate(0, 0, speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            //transform.position -= new Vector3(0, 0,   speed);
+            transform.Translate(0,0, -speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.eulerAngles += new Vector3(0, rotationSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.eulerAngles -= new Vector3(0, rotationSpeed, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject clon;
+            clon = Instantiate(prefab);
+            clon.transform.position = transform.position - new Vector3(0, 0, -1);
+            clon.transform.localScale = newScale *= 1.1f;
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "EnablerCube")
+        {
+            isAble = true;
+        }else if (col.gameObject.name == "DisablerCube")
+        {
+            isAble = false;
+        }     
+    }
+}
---
+++ Assets/TestCollision.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter()
+    {
+        Debug.Log("Contacto");
+        transform.position += new Vector3(0, 2, 0);
+    }
+
+    void OnCollisionExit()
+    {
+        Debug.Log("Dejo de hacer Contacto");
+    }
+
+    void OnCollisionStay()
+    {
+        Debug.Log("Está haciendo Contacto");
+    }
+}
